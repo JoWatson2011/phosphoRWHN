75c118dfd3c54d1ce2281c5aba53b782e73f0472
Jo <jowatson2011@hotmail.co.uk>
2021-05-17 14:23:41 +0100

    Whilst debugging API - use flat files

--- R/constructHetNet.R
+++ R/constructHetNet.R
@@ -28,20 +28,22 @@
 constructHetNet <- function(clustering, phosphoData = NULL,
                             enrichrLib = "GO_Biological_Process_2018",
                             stringConf = 0.4,
-                            modules = T, pval = 0.05, simplify = T){
+                            modules = T, pval = 0.05, simplify = T,
+                            API = F){
 
   if(is.null(names(clustering))){
     stop("Vector must be named with gene names + modified residue (see documentation)")
   }
 
   ## Phospho
-  phos <- lapply(unique(clustering)[order(unique(clustering))], function(x){
+  phos <- lapply(unique(clustering)[order(unique(clustering))],
+                 function(x){
     cl <- names(clustering[clustering == x])
 
     if(is.null(phosphoData)){
       xpnd <- expand.grid(cl, cl)
     }else{
-      if(colnames(phosphoData)[1] != "id" | class(phosphoData[,1] != "character")){
+      if(colnames(phosphoData)[1] != "id" | class(phosphoData$id) != "character"){
         stop("phosphoData must have id (gene names + modified residue, as described in documentation) as first column")
       }
 
@@ -86,78 +88,79 @@ constructHetNet <- function(clustering, phosphoData = NULL,
   #   }
   # prot <- STRING.expmt.gene[(STRING.expmt.gene$protein1 %in% prots) | (STRING.expmt.gene$protein2 %in% prots), c(2:3)] %>%
 
+  if(API == T){
+    STRING_api_interactors <-
+      httr::POST(url = "https://version-11-0b.string-db.org/api/tsv/interaction_partners",
+                 body = list(
+                   identifiers = paste(prots, collapse = "%0d"),
+                   required_score = stringConf,
+                   species = "9606"#,
+                   # caller_identity = "phosphoRWHN"
+                 )
+      )
 
-  STRING_api_interactors <-
-    httr::POST("https://version-11-0b.string-db.org/api/tsv/interaction_partners",
-                       body = list(
-                         identifiers = paste(prots, collapse = "%0d"),
-                         required_score = 1,
-                         species = "9606"
-                       )
+    if (httr::http_error(STRING_api_interactors)) {
+      stop(
+        sprintf(
+          "STRING API request failed [%s]",
+          httr::status_code(STRING_api_interactors)
+        ),
+        call. = F
+      )
+    }
+
+    STRING_api_interactors <- suppressMessages(
+      httr::content(STRING_api_interactors)
     )
-
-  if (httr::http_error(STRING_api_interactors)) {
-    stop(
-      sprintf(
-        "STRING API request failed [%s]",
+    if(nrow(dplyr::filter(STRING_api_interactors,
+                          .data$escore >= stringConf)) > 0){
+      STRING_api_interactors <- dplyr::filter(STRING_api_interactors,
+                                              .data$escore >= stringConf)
+    }else{
+      stop(sprintf(
+        "No PPI interactions at STRING confidence score %s. Set confidence score lower.",
         httr::status_code(STRING_api_interactors)
       ),
       call. = F
-    )
-  }
+      )
+    }
 
-  STRING_api_interactors <- suppressMessages(
-    httr::content(STRING_api_interactors)
-  )
-  if(nrow(dplyr::filter(STRING_api_interactors,
-                        .data$escore >= stringConf)) > 0){
-    STRING_api_interactors <- dplyr::filter(STRING_api_interactors,
-                                            .data$escore >= stringConf)
-  }else{
-    stop(sprintf(
-      "No PPI interactions at STRING confidence score %s. Set confidence score lower.",
-      httr::status_code(STRING_api_interactors)
-    ),
-    call. = F
-    )
-  }
+    query_size <- 1999 - length(prots)
 
+    STRING_api_interactors$group <- if(nrow(STRING_api_interactors) > 1999){ # Max. queries to STRING api = 2000
+      c(                                                                     # Split df into groups of 1999
+        as.vector(                                                           #    to be queried separately
+          sapply(
+            seq(query_size, nrow(STRING_api_interactors) -
+                  (nrow(STRING_api_interactors) %% 1991), query_size),
+            rep.int, times = query_size
+          )
+        ),
+        rep.int(nrow(STRING_api_interactors), times = nrow(STRING_api_interactors) %% query_size)
+      )
+    } else {
+      rep(1, nrow(STRING_api_interactors))
 
-
-  query_size <- 1999 - length(prots)
-
-  STRING_api_interactors$group <- if(nrow(STRING_api_interactors) > 1999){ # Max. queries to STRING api = 2000
-    c(                                                                     # Split df into groups of 1999
-      as.vector(                                                           #    to be queried separately
-        sapply(
-          seq(query_size, nrow(STRING_api_interactors) -
-                (nrow(STRING_api_interactors) %% 1991), query_size),
-          rep.int, times = query_size
-        )
-      ),
-      rep.int(nrow(STRING_api_interactors), times = nrow(STRING_api_interactors) %% query_size)
-    )
-  } else {
-    rep(1, nrow(STRING_api_interactors))
-
-  }
+    }
 
 
     STRING_api_nw <- lapply(
       unique(STRING_api_interactors$group), function(i){
+        Sys.sleep(1)
         interactors <- STRING_api_interactors[STRING_api_interactors$group == i,]$preferredName_B
 
         STRING_api_nw_part <- httr::POST("https://version-11-0b.string-db.org/api/tsv/network",
-                                    body = list(
-                                      identifiers = paste(c(prots,
-                                                            interactors),
-                                      collapse = "%0d"
-                                      ),
-                                      required_score = 1,
-                                      species = "9606"
-                                    )
+                                         body = list(
+                                           identifiers = paste(c(prots,
+                                                                 interactors),
+                                                               collapse = "%0d"
+                                           ),
+                                           required_score = 1,
+                                           species = "9606",
+                                           caller_identity = "phosphoRWHN"
+                                         )
         )
-        Sys.sleep(1)
+
         if (httr::http_error(STRING_api_nw_part)) {
           stop(
             sprintf(
@@ -171,30 +174,42 @@ constructHetNet <- function(clustering, phosphoData = NULL,
         STRING_api_nw_part <- suppressMessages(httr::content(STRING_api_nw_part))
 
         return(STRING_api_nw_part)
-    }) %>%
+      }) %>%
       dplyr::bind_rows() %>%
       unique()
 
 
-  prot <- STRING_api_nw %>%
-    dplyr::filter(.data$escore >= stringConf) %>%
-    dplyr::select(.data$preferredName_A, .data$preferredName_B) %>%
-    na.omit() %>%
-    igraph::graph_from_data_frame(directed = F) %>%
-    igraph::simplify(remove.multiple = F,remove.loops = T) %>%
-    igraph::as_data_frame() %>%
-    dplyr::select(prot1 = .data$from, prot2 = .data$to)
+    prot <- STRING_api_nw %>%
+      dplyr::filter(.data$escore >= stringConf) %>%
+      dplyr::select(.data$preferredName_A, .data$preferredName_B) %>%
+      na.omit() %>%
+      igraph::graph_from_data_frame(directed = F) %>%
+      igraph::simplify(remove.multiple = F,remove.loops = T) %>%
+      igraph::as_data_frame() %>%
+      dplyr::select(prot1 = .data$from, prot2 = .data$to)
+  }else{
+    prot <- if(stringConf < 0.6){STRING_lowconf}else{STRING_highconf}
+    prot <- prot %>%
+      dplyr::filter(.data$protein1 %in% prots | .data$protein2 %in% prots) %>%
+      dplyr::filter(.data$experimental >= stringConf) %>%
+      dplyr::select(protein1, protein2) %>%
+      na.omit() %>%
+      igraph::graph_from_data_frame(directed = F) %>%
+      igraph::simplify(remove.multiple = F,remove.loops = T) %>%
+      igraph::as_data_frame() %>%
+      dplyr::select(prot1 = .data$from, prot2 = .data$to)
+  }
 
   ## Protein >> Func
 
-  enrichedTerms <- if(length(prots) > 25){
+  enrichedTerms <- #if(length(prots) > 25){
     lapply(unique(clustering)[order(unique(clustering))], function(i){
       ids <- names(clustering[clustering == i ] )
       cl_prots <- gsub("_.*", "", ids)
 
       enrichedTerms <- if(modules){
-        el <- STRING.expmt.gene %>%
-          dplyr::filter(.data$protein1 %in% cl_prots & .data$protein2 %in% cl_prots) %>%
+        el <- prot %>%
+          dplyr::filter(.data$prot %in% cl_prots & .data$prot %in% cl_prots) %>%
           dplyr::select(2:3) %>%
           na.omit()
         nw <- igraph::graph_from_data_frame(el, directed = F) %>%
@@ -226,11 +241,11 @@ constructHetNet <- function(clustering, phosphoData = NULL,
                                     pval = pval)
       }
     }) %>% dplyr::bind_rows()
-  } else{
-    enrichedTerms <- getEnrichr(unique(prots),
-                                enrichrLib = enrichrLib,
-                                pval = pval)
-  }
+  # } else{
+  #   enrichedTerms <- getEnrichr(unique(prots),
+  #                               enrichrLib = enrichrLib,
+  #                               pval = pval)
+  # }
 
 
 
@@ -244,8 +259,24 @@ constructHetNet <- function(clustering, phosphoData = NULL,
                                "",
                                .data$GOID))
     if(simplify == T){
+      hsGO <- suppressMessages(
+        GOSemSim::godata('org.Hs.eg.db',
+                         ont = ifelse(
+                           grepl("Biological", enrichrLib),
+                           "BP",
+                           ifelse(grepl("Molecular", enrichrLib), "MF",
+                                  ifelse(grepl(
+                                    "Cellular", enrichrLib
+                                  ), "CC",
+                                  NA))
+                         ),
+                         computeIC=FALSE
+        )
+      )
+
       keepID <- simplifyGO(GOID = enrichedTerms$GOID,
-                           simplifyData = simpleGO)
+                           highFreqTerms = highFreqTerms,
+                           semData = hsGO)
     }else{
       keepID <- enrichedTerms$GOID
     }
@@ -264,6 +295,8 @@ constructHetNet <- function(clustering, phosphoData = NULL,
 
   ## Func
   if(grepl("GO", enrichrLib)){
+
+    if(!exists("hsGO")){
     hsGO <- suppressMessages(
       GOSemSim::godata('org.Hs.eg.db',
                        ont = ifelse(
@@ -276,8 +309,9 @@ constructHetNet <- function(clustering, phosphoData = NULL,
                                 NA))
                        ),
                        computeIC=FALSE
-                       )
+      )
     )
+    }
     semSim <- expand.grid(prot_func$ID, prot_func$ID, stringsAsFactors = F)
 
     semSim$sim <- apply(semSim, 1, function(i) GOSemSim::goSim(i[1], i[2], semData = hsGO, measure = "Wang"))
@@ -294,25 +328,6 @@ constructHetNet <- function(clustering, phosphoData = NULL,
       unique() %>%
       na.omit()
   }else{
-
-
-    #The .rds file is the output of the `all edges bma wang.txt` file
-    # (found in the supplementary materials of:
-    #
-    # Stoney RA, Ames RM, Nenadic G, Robertson DL, Schwartz JM.
-    # Disentangling the multigenic and pleiotropic nature of molecular function.
-    # BMC Syst Biol. 2015;9 Suppl 6(Suppl 6):S3. doi:10.1186/1752-0509-9-S6-S3)
-    #
-    # and the following code:
-
-    # pwnet <- readr::read_tsv("data/all edges bma wang.txt",
-    #                                   col_names = c("sim", "func1", "func2")) %>%
-    #   mutate(func1 = tolower(func1),
-    #          func2 = tolower(func2)) %>%
-    #   mutate(func1 = gsub("_", " ", func1),
-    #          func2 = gsub("_", " ", func2)
-    #   )
-    # saveRDS(pwnet, "data/pathwaySimilarities_Stoney2015.rds")
 
     func <- pathwaySimilarities %>%
       dplyr::filter(.data$func1 %in% enrichedTerms$Term &
